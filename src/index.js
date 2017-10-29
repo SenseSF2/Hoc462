69776ccdea58e5d8543dd634fa8fab86e7ebabd0
khanh2003 <huynhtrankhanh2003@gmail.com>
2017-10-29 16:02:52 +0700

    Changed title

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Drawer from './components/Drawer'
 import Renderer from './components/Renderer'
+document.title = 'Hoc462 Room Planner'
 const root = document.getElementById('root')
 root.appendChild(Header())
 root.appendChild(Sidebar())
