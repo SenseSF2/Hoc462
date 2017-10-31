aca470c484ffa5ef4d8f4e33d12b7f5c6e99b7f7
khanh2003 <huynhtrankhanh2003@gmail.com>
2017-10-31 21:03:42 +0700

    Added a stack trace to aid debugging

--- src/EventBus.js
+++ src/EventBus.js
@@ -1,1 +1,8 @@
-export default document.createElement('event-bus')
+const bus = document.createElement('event-bus')
+const { dispatchEvent } = bus
+bus.dispatchEvent = (...args) => {
+  console.log('The following event is dispatched: ', args[0].type, args)
+  console.log('Event stack trace: ', new Error())
+  return dispatchEvent.bind(bus)(...args)
+}
+export default bus
