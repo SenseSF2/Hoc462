c6259932043cb393edfd6c1d6ac719ba82767644
khanh2003 <huynhtrankhanh2003@gmail.com>
2017-12-11 14:34:53 +0700

    Partially support saving presentation.

--- src/EventBus.js
+++ src/EventBus.js
@@ -1,10 +1,13 @@
 const bus = document.createElement('event-bus')
 const { dispatchEvent } = bus
+let events = []
 bus.dispatchEvent = (...args) => {
+  events = [...events, { type: args[0].type, detail: args[0].detail }]
   console.log('The following event is dispatched: ', args[0].type, args)
   console.log('Event stack trace: ', new Error())
   return dispatchEvent.bind(bus)(...args)
 }
-"REMOVE"
 window.bus = bus
+const getEvents = () => events
 export default bus
+export { getEvents }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,50 @@
+import EventBus, { getEvents } from '../EventBus'
 import styles from './Header.css'
+import { button } from './Button.css'
+import playAllSlides from '../actions/playAllSlides'
 export default () => {
   const root = document.createElement('div')
   root.classList.add(styles.header)
   root.innerHTML = `
-    <h1>Hoc462 Room Planner</h1>
+    <h1>
+      Hoc462 Room Planner
+      <button class="${button} save">Save</button>
+      <button class="${button} load">Load</button>
+      <button class="${button} slide-show">Slide show</button>
+      <input type="file" class="file-picker" style="display: none">
+    </h1>
   `
+  const saveButton = root.querySelector('.save')
+  const loadButton = root.querySelector('.load')
+  saveButton.addEventListener('click', () => {
+    const blob = new window.Blob(
+      [JSON.stringify(getEvents())], { type: 'text/plain' }
+    )
+    const aElement = document.createElement('a')
+    const downloadUrl = window.URL.createObjectURL(blob)
+    aElement.setAttribute('href', downloadUrl)
+    aElement.setAttribute('download', 'untitled.hoc462presentation')
+    aElement.click()
+  })
+  const filePickerElement = root.querySelector('.file-picker')
+  loadButton.addEventListener('click', () => {
+    filePickerElement.click()
+  })
+  filePickerElement.addEventListener('change', () => {
+    const reader = new window.FileReader()
+    reader.readAsText(filePickerElement.files[0])
+    reader.addEventListener('loadend', () => {
+      const events = JSON.parse(reader.result)
+      for (let event of events) {
+        EventBus.dispatchEvent(
+          new window.CustomEvent(event.type, { detail: event.detail })
+        )
+      }
+    })
+  })
+  const slideShowButton = root.querySelector('.slide-show')
+  slideShowButton.addEventListener('click', () => {
+    EventBus.dispatchEvent(playAllSlides())
+  })
   return root
 }
