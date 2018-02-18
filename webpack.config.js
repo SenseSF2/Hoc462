2ed8f450c33dfd9efe61d9302616fc50c633ee60
khanh2003 <huynhtrankhanh2003@gmail.com>
2018-02-18 15:06:46 +0700

    Move Babel config to .babelrc file

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env', 'react'],
-            plugins: ['transform-decorators-legacy', 'transform-object-rest-spread', 'transform-class-properties']
-          }
-        }
+        use: { loader: 'babel-loader' }
       }
     ]
   },
