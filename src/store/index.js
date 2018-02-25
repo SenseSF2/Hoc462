ba0cc0694d6c3b9b129e53904219e2127ba3937e
khanh2003 <huynhtrankhanh2003@gmail.com>
2018-02-25 09:56:15 +0700

    Select Slide tab when a slide is selected

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
 import List from "./List";
 import UIState from "./UIState";
+import { SLIDE } from "../constants";
 class RootStore {
   constructor() {
     this.objects = new List();
-    this.slides = new List();
+    this.slides = new List({
+      add: original => () => {
+        original();
+        this.uiState.selectDrawerTab(SLIDE);
+      },
+      select: original => () => {
+        original();
+        this.uiState.selectDrawerTab(SLIDE);
+      }
+    });
     this.uiState = new UIState(this);
   }
 }
